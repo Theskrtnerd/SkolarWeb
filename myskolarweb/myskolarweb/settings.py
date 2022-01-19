f43f0aea53171dd285f01333591bf137e6bc525f
bach <tvbbd2@gmail.com>
2022-01-19 22:23:40 +0700

    version 0.2

--- myskolarweb/myskolarweb/settings.py
+++ myskolarweb/myskolarweb/settings.py
@@ -118,11 +118,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
 
-MEDIA_URL = '/media/'
+
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 PAGEDOWN_IMAGE_UPLOAD_ENABLED=True
 
